831070aa9c19c752466d0cf165e039c065ea5e17
Vishal Chiluveri <vishalchiluveri@gmail.com>
2020-06-13 13:57:31 +0530

    Created Button Component and updated its story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {action} from '@storybook/addon-actions';
-import {withKnobs, text} from '@storybook/addon-knobs/react';
+import {withKnobs, text, select, number} from '@storybook/addon-knobs/react';
 
 import Button from './';
 
@@ -13,25 +13,371 @@ export default {
     },
 };
 
-export const Example = () => {
+const PaddingWrap = ({children}) => {
     return (
-        <Button onClick={action('button clicked')}>
-            Example
-        </Button>
+        <div style={{display: "inline-block", padding: "5px"}}>
+            {children}
+        </div>
     )
 };
 
-export const Example2 = () => {
+export const Colors = () => {
     return (
-        <Button onClick={action('button clicked')}>
-            Example2
-        </Button>
+        <>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "primary",
+                }}>
+                    Primary
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "secondary",
+                }}>
+                    Secondary
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "neutral",
+                }}>
+                    Neutral
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "error",
+                }}>
+                    Error
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "warning",
+                }}>
+                    Warning
+                </Button>
+            </PaddingWrap>
+            <br/>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "primary",
+                    buttonStyle: "outline"
+                }}>
+                    Primary
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "secondary",
+                    buttonStyle: "outline"
+                }}>
+                    Secondary
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "neutral",
+                    buttonStyle: "outline"
+                }}>
+                    Neutral
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "error",
+                    buttonStyle: "outline"
+                }}>
+                    Error
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    color: "warning",
+                    buttonStyle: "outline"
+                }}>
+                    Warning
+                </Button>
+            </PaddingWrap>
+        </>
+    )
+};
+
+export const Sizes = () => {
+    return (
+        <>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "xs"
+                }}>
+                    Extra Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "sm"
+                }}>
+                    Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "md"
+                }}>
+                    Medium
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "lg"
+                }}>
+                    Large
+                </Button>
+            </PaddingWrap>
+            <br/>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "xs",
+                    buttonStyle: "outline"
+                }}>
+                    Extra Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "sm",
+                    buttonStyle: "outline"
+                }}>
+                    Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "md",
+                    buttonStyle: "outline"
+                }}>
+                    Medium
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "lg",
+                    buttonStyle: "outline"
+                }}>
+                    Large
+                </Button>
+            </PaddingWrap>
+        </>
+    )
+};
+
+export const ColorWeights = () => {
+    return (
+        <>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 100
+                }}>
+                    100
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 200
+                }}>
+                    200
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 300
+                }}>
+                    300
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 400
+                }}>
+                    400
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 500
+                }}>
+                    500
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 600
+                }}>
+                    600
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 700
+                }}>
+                    700
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 800
+                }}>
+                    800
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 900
+                }}>
+                    900
+                </Button>
+            </PaddingWrap>
+            <br/>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 500,
+                    buttonStyle: "outline"
+                }}>
+                    500
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 600,
+                    buttonStyle: "outline"
+                }}>
+                    600
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 700,
+                    buttonStyle: "outline"
+                }}>
+                    700
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 800,
+                    buttonStyle: "outline"
+                }}>
+                    800
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    colorWeight: 900,
+                    buttonStyle: "outline"
+                }}>
+                    900
+                </Button>
+            </PaddingWrap>
+        </>
+    )
+};
+
+export const Rounded = () => {
+    return (
+        <>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "xs",
+                    border: "rounded"
+                }}>
+                    Extra Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "sm",
+                    border: "rounded"
+                }}>
+                    Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "md",
+                    border: "rounded"
+                }}>
+                    Medium
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "lg",
+                    border: "rounded"
+                }}>
+                    Large
+                </Button>
+            </PaddingWrap>
+            <br/>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "xs",
+                    buttonStyle: "outline",
+                    border: "rounded"
+                }}>
+                    Extra Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "sm",
+                    buttonStyle: "outline",
+                    border: "rounded"
+                }}>
+                    Small
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "md",
+                    buttonStyle: "outline",
+                    border: "rounded"
+                }}>
+                    Medium
+                </Button>
+            </PaddingWrap>
+            <PaddingWrap>
+                <Button onClick={action('button clicked')} theme={{
+                    size: "lg",
+                    buttonStyle: "outline",
+                    border: "rounded"
+                }}>
+                    Large
+                </Button>
+            </PaddingWrap>
+        </>
     )
 };
 
 export const ExampleWithKnobs = () => {
     return (
-        <Button onClick={action('button clicked')}>
+        <Button onClick={action('button clicked')} theme={{
+            color: select("color", ["primary", "secondary", "neutral", "error", "warning"]),
+            colorWeight: number("colorWeight", 700, {
+                range: true,
+                min: 100,
+                max: 900,
+                step: 100,
+            }),
+            buttonStyle: select("buttonStyle", ["normal", "outline"]),
+            size: select("size", {
+                "Extra Small": "xs",
+                "Small": "sm",
+                "Medium": "md",
+                "Large": "lg",
+            }, "md"),
+            border: select("border", ["flat", "rounded"]),
+        }}>
             {text('children', "hello")}
         </Button>
     );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,17 +6,26 @@ import * as PropTypes from 'prop-types';
  *- Use an avatar for attributing actions or content to specific users.
  *- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
  **/
-export default function Button({children, ...props}) {
+export default function Button({children, theme, ...props}) {
+    const {color = "primary", colorWeight = 700, buttonStyle = "normal", size = "md", border = "flat"} = theme;
+
+    const className = classNames(
+        'border',
+        {[`border-${color}-${colorWeight}`]: buttonStyle === "normal"},
+        {[`bg-${color}-${colorWeight}`]: buttonStyle === "normal"},
+        {[`text-on_${color}-${colorWeight}`]: buttonStyle === "normal"},
+        {[`border-${color}-${colorWeight}`]: buttonStyle === "outline"},
+        {[`text-${color}-${colorWeight}`]: buttonStyle === "outline"},
+        {'rounded-md': border === "rounded"},
+        'shadow-md',
+        'px-5 py-1',
+        `text-${size}`
+    );
+
     return (
         <button
             {...props}
-            className={classNames(
-                'border border-gray-500',
-                'p-1 px-3',
-                'text-gray-500',
-                'hover:shadow-md',
-                'focus:outline-none'
-            )}
+            className={className}
         >
             {children}
         </button>
@@ -28,8 +37,19 @@ Button.propTypes = {
      * Here the documentation goes of each prop
      */
     children: PropTypes.any,
+    /**
+     * Set the theme of the Button using this prop
+     */
+    theme: PropTypes.shape({
+        color: PropTypes.oneOf(["primary", "secondary", "neutral", "error", "warning"]),
+        colorWeight: PropTypes.oneOf([100, 200, 300, 400, 500, 600, 700, 800, 900]),
+        buttonStyle: PropTypes.oneOf(["normal", "outline"]),
+        size: PropTypes.oneOf(["xs", "sm", "md", "lg"]),
+        border: PropTypes.oneOf(["flat", "rounded"]),
+    })
 };
 
 Button.defaultProps = {
     children: "Button",
+    theme: {color: "primary", colorWeight: 900, buttonStyle: "normal", size: "md", border: "flat"}
 };
