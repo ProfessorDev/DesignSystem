dbe7fbda3c499b9cb4a76cdab72831db171b87c5
Vishal Chiluveri <vishalchiluveri@gmail.com>
2020-06-19 19:36:03 +0530

    Button Component file renamed from index.js to Button.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 
 function App() {
     return (
