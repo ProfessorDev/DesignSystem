1b99cf007c4df9137238d89b710a5909560672cc
Vishal Chiluveri <36564980+ironmanvim@users.noreply.github.com>
2020-12-16 09:51:20 +0000

    Added Badge Component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import twx from "tailwindcssx";
+
+export interface BadgeProps {
+    content: string,
+    variant?: string,
+    icon?: React.ReactNode,
+    iconAlignment?: "left" | "right",
+};
+
+export const Badge: React.FC<BadgeProps> = ({ content, variant = "neutral", icon, iconAlignment = "left" }) => {
+    const iconWrapper = icon && (
+        <span className={twx([
+            iconAlignment === "left" && "mr-1",
+            iconAlignment === "right" && "ml-1",
+            'align-middle'
+        ])}>
+            {icon}
+        </span>
+    );
+
+    return (
+        <span className={twx([
+            'text-sm',
+            'font-semibold',
+            'p-0.5',
+            'px-2',
+            // 'border border-red-600',
+            'rounded-full',
+            (variant === "error" || variant === "outline-error") && "border border-red-600",
+            (variant === "warning" || variant === "outline-warning") && "border border-yellow-600",
+            (variant === "success" || variant === "outline-success") && "border border-green-600",
+            (variant === "neutral" || variant === "outline-neutral") && "border border-gray-600",
+            variant === "error" && "bg-red-600 text-white",
+            variant === "warning" && "bg-yellow-600 text-white",
+            variant === "success" && "bg-green-600 text-white",
+            variant === "neutral" && "bg-gray-600 text-white",
+            variant === "outline-error" && "text-red-600",
+            variant === "outline-warning" && "text-yellow-600",
+            variant === "outline-success" && "text-green-600",
+            variant === "outline-neutral" && "text-gray-600",
+        ])}>
+            {iconAlignment === "left" && (
+                <>
+                    {iconWrapper}
+                    {content}
+                </>
+            )}
+            {iconAlignment === "right" && (
+                <>
+                    {content}
+                    {iconWrapper}
+                </>
+            )}
+        </span>
+    );
+};
---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, Story } from "@storybook/react";
+import { Badge, BadgeProps } from "../components/Badge/Badge";
+import React from "react";
+import twx from "tailwindcssx";
+
+export default {
+    title: "Design System/Badge",
+} as Meta;
+
+const Template: Story<BadgeProps> = (args) => (
+    <Badge {...args} />
+);
+
+export const LeftIcon = Template.bind({});
+LeftIcon.args = {
+    content: "Hello World",
+    icon: <i className="fas fa-exclamation-triangle" />,
+    iconAlignment: "left"
+}
+
+export const RightIcon = Template.bind({});
+RightIcon.args = {
+    content: "Hello World",
+    icon: <i className="fas fa-exclamation-triangle" />,
+    iconAlignment: "right",
+    variant: "error",
+}
+
+export const NoIcon = Template.bind({});
+NoIcon.args = {
+    content: "Hello World",
+    iconAlignment: "right"
+}
+
+export const Colors: Story = () => {
+    return (
+        <div className={twx([
+            'space-y-2',
+        ])}>
+            <div className={twx([
+                'space-x-2',
+            ])}>
+                <Badge content="Hello World" variant="error" />
+                <Badge content="Hello World" variant="success" />
+                <Badge content="Hello World" variant="warning" />
+                <Badge content="Hello World" variant="neutral" />
+            </div>
+            <div className={twx([
+                'space-x-2',
+            ])}>
+                <Badge content="Hello World" variant="outline-error" />
+                <Badge content="Hello World" variant="outline-success" />
+                <Badge content="Hello World" variant="outline-warning" />
+                <Badge content="Hello World" variant="outline-neutral" />
+            </div>
+        </div>
+    )
+}
