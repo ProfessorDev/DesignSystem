fc8ce409e04867aec3498d5eab83ea7fb0f66687
Vishal Chiluveri <36564980+ironmanvim@users.noreply.github.com>
2020-12-25 18:14:58 +0000

    updates - removed @bit/ironmanvim.comp.fontawesome package - added @fortawesome/fontawesome-free - moved typescript to devDependencies

--- src/cssImport.js
+++ src/cssImport.js
@@ -2,7 +2,7 @@
  * Use this file to import css in the React App
  */
 import './assets/css/build/tailwind.css';
-import '@bit/ironmanvim.comp.fontawesome/css/all.css';
+import '@fortawesome/fontawesome-free/js/all';
 import './assets/css/build/directional-arrows.css';
 import './assets/css/build/arrow-position.css';
 import './assets/css/build/dropdown-position.css';
